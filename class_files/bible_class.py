1c8ad69776f947751145b53fab3e9c72b309de42
Luke Brady <luke@isajediknight.com>
2018-02-21 00:13:07 -0600

    Creating the Bible Class file
    This will have all the attributes needed

---
+++ class_files/bible_class.py
@@ -0,0 +1,136 @@
+
+class bible:
+
+    book_to_chapter = {}
+    book_to_chapter['Genesis'] = 50
+    book_to_chapter['Exodus'] = 40
+    book_to_chapter['Leviticus'] = 27
+    book_to_chapter['Numbers'] = 36
+    book_to_chapter['Deuteronomy'] = 34
+    book_to_chapter['Joshua'] = 24
+    book_to_chapter['Judges'] = 21
+    book_to_chapter['Ruth'] = 4
+    book_to_chapter['1 Samuel'] = 31
+    book_to_chapter['2 Samuel'] = 24
+    book_to_chapter['1 Kings'] = 22
+    book_to_chapter['2 Kings'] = 25
+    book_to_chapter['1 Chronicles'] = 29
+    book_to_chapter['2 Chronicles'] = 36
+    book_to_chapter['Ezra'] = 10
+    book_to_chapter['Nehemiah'] = 13
+    book_to_chapter['Esther'] = 10
+    book_to_chapter['Job'] = 42
+    book_to_chapter['Psalms'] = 150
+    book_to_chapter['Proverbs'] = 31
+    book_to_chapter['Ecclesiastes'] = 12
+    book_to_chapter['Song of Solomon'] = 8
+    book_to_chapter['Isaiah'] = 66
+    book_to_chapter['Jeremiah'] = 52
+    book_to_chapter['Lamentations'] = 5
+    book_to_chapter['Ezekiel'] = 48
+    book_to_chapter['Daniel'] = 12
+    book_to_chapter['Hosea'] = 14
+    book_to_chapter['Joel'] = 3
+    book_to_chapter['Amos'] = 9
+    book_to_chapter['Obadiah'] = 1
+    book_to_chapter['Jonah'] = 4
+    book_to_chapter['Micah'] = 7
+    book_to_chapter['Nahum'] = 3
+    book_to_chapter['Habakkuk'] = 3
+    book_to_chapter['Zephaniah'] = 3
+    book_to_chapter['Haggai'] = 2
+    book_to_chapter['Zechariah'] = 14
+    book_to_chapter['Malachi'] = 4
+    book_to_chapter['Matthew'] = 28
+    book_to_chapter['Mark'] = 16
+    book_to_chapter['Luke'] = 24
+    book_to_chapter['John'] = 21
+    book_to_chapter['Romans'] = 16
+    book_to_chapter['1 Corinthians'] = 16
+    book_to_chapter['2 Corinthians'] = 13
+    book_to_chapter['Galatians'] = 6
+    book_to_chapter['Ephesians'] = 6
+    book_to_chapter['Philippians'] = 4
+    book_to_chapter['Colossians'] = 4
+    book_to_chapter['1 Thessalonians'] = 5
+    book_to_chapter['2 Thessalonians'] = 3
+    book_to_chapter['1 Timothy'] = 6
+    book_to_chapter['2 Timothy'] = 4
+    book_to_chapter['Titus'] = 3
+    book_to_chapter['Philemon'] = 1
+    book_to_chapter['Hebrews'] = 13
+    book_to_chapter['James'] = 5
+    book_to_chapter['1 Peter'] = 5
+    book_to_chapter['2 Peter'] = 3
+    book_to_chapter['1 John'] = 5
+    book_to_chapter['2 John'] = 1
+    book_to_chapter['3 John'] = 1
+    book_to_chapter['Jude'] = 1
+    book_to_chapter['Revelation'] = 22
+
+    book_order = []
+    book_order.append('Genesis')
+    book_order.append('Exodus')
+    book_order.append('Leviticus')
+    book_order.append('Numbers')
+    book_order.append('Deuteronomy')
+    book_order.append('Joshua')
+    book_order.append('Judges')
+    book_order.append('Ruth')
+    book_order.append('1 Samuel')
+    book_order.append('2 Samuel')
+    book_order.append('1 Kings')
+    book_order.append('2 Kings')
+    book_order.append('1 Chronicles')
+    book_order.append('2 Chronicles')
+    book_order.append('Ezra')
+    book_order.append('Nehemiah')
+    book_order.append('Esther')
+    book_order.append('Job')
+    book_order.append('Psalms')
+    book_order.append('Proverbs')
+    book_order.append('Ecclesiastes')
+    book_order.append('Song of Solomon')
+    book_order.append('Isaiah')
+    book_order.append('Jeremiah')
+    book_order.append('Lamentations')
+    book_order.append('Ezekiel')
+    book_order.append('Daniel')
+    book_order.append('Hosea')
+    book_order.append('Joel')
+    book_order.append('Amos')
+    book_order.append('Obadiah')
+    book_order.append('Jonah')
+    book_order.append('Micah')
+    book_order.append('Nahum')
+    book_order.append('Habakkuk')
+    book_order.append('Zephaniah')
+    book_order.append('Haggai')
+    book_order.append('Zechariah')
+    book_order.append('Malachi')
+    book_order.append('Matthew')
+    book_order.append('Mark')
+    book_order.append('Luke')
+    book_order.append('John')
+    book_order.append('Romans')
+    book_order.append('1 Corinthians')
+    book_order.append('2 Corinthians')
+    book_order.append('Galatians')
+    book_order.append('Ephesians')
+    book_order.append('Philippians')
+    book_order.append('Colossians')
+    book_order.append('1 Thessalonians')
+    book_order.append('2 Thessalonians')
+    book_order.append('1 Timothy')
+    book_order.append('2 Timothy')
+    book_order.append('Titus')
+    book_order.append('Philemon')
+    book_order.append('Hebrews')
+    book_order.append('James')
+    book_order.append('1 Peter')
+    book_order.append('2 Peter')
+    book_order.append('1 John')
+    book_order.append('2 John')
+    book_order.append('3 John')
+    book_order.append('Jude')
+    book_order.append('Revelation')
